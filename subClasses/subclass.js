be20ab0f6d1440b49820be8f13643bc6691b216f
Henry Richardson <hrichardson83@gmail.com>
2018-11-07 23:28:21 -0500

    added sub class bat

--- subClasses/subclass.js
+++ subClasses/subclass.js
@@ -8,3 +8,17 @@ class Mammal {
         return `$(this.name) is walking`
     }
 }
+
+class Bat extends Mammal{
+    constructor(_legs, _name, _eatsMeat){
+        super(_legs, _name);
+        this.eatsMeat = _eatsMeat;
+    }
+    fly(){
+        return `${this.name} is flying`;
+    }
+    walk(){
+        let holding = this.eatsMeat ? 'bug' : 'carrot';
+        return `${super.walk()} with a ${holding}`;
+    }
+}
