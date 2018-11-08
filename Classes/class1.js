57cfb90715cfa3933aa6e2511948c5bda02d6343
Henry Richardson <hrichardson83@gmail.com>
2018-11-07 23:09:51 -0500

    class simulation

--- Classes/class1.js
+++ Classes/class1.js
@@ -3,4 +3,12 @@ class Car {
         this.color = color;
     }
 }
+// returns a function
 console.dir(typeof(Car));
+
+let Car = (function(){
+    let _car = function(color){
+        this.color = color;
+    }
+})();
+
