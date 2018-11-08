292482eda2e22551bca5823e3c3e79576910cca5
Henry Richardson <hrichardson83@gmail.com>
2018-11-07 23:11:12 -0500

    seperated proto

--- Classes/class1.js
+++ Classes/class1.js
@@ -2,13 +2,10 @@ class Car {
     constructor(color){
         this.color = color;
     }
+
 }
 // returns a function
 console.dir(typeof(Car));
 
-let Car = (function(){
-    let _car = function(color){
-        this.color = color;
-    }
-})();
+
 
---
+++ Classes/proto.js
@@ -0,0 +1,7 @@
+let Car = (function(){
+    let _car = function(color){
+        this.color = color;
+    }
+})();
+
+console.dir(Car);
